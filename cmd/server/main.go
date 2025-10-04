ec4b272438b3275501365d993abc5cb1356ecbbe
guarref <prostoboh902@gmail.com>
2025-10-04 21:54:23 +0300

    updated enter main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/guarref/users-service/internal/database"
 	"github.com/guarref/users-service/internal/transport/grpc"
@@ -10,10 +13,18 @@ import (
 
 func main() {
 	database.InitDB()
+	err := database.DB.AutoMigrate(&user.User{})
+	if err != nil {
+		log.Fatalf("failed to automigrate users table: %v", err)
+	}
 	repo := user.NewUserRepository(database.DB)
 	svc := user.NewUserService(repo)
 
 	if err := grpc.RunGRPC(svc); err != nil {
 		log.Fatalf("gRPC сервер завершился с ошибкой: %v", err)
 	}
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+	<-sigCh
 }
